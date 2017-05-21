2f31479e70e6a09b99763151b4f49a269b8477f1
Dan Zygmund-Felt <danzygmund-felt@zipcoder10.local>
2017-05-21 15:41:03 -0400

    Implemented several methods in MySet. No idea yet if tests will pass.

--- src/main/java/zygmundfelt/dan/generics/MySet.java
+++ src/main/java/zygmundfelt/dan/generics/MySet.java
@@ -6,19 +6,52 @@ import java.util.Set;
 
 public class MySet<E> implements Set<E> {
 
-    public boolean add(E element) {
-        return false;
+    private Object[] mySet;
+    private int size;
+    private static final int MINIMUM_SIZE = 10;
+
+    MySet() {
+        mySet = new Object[MINIMUM_SIZE];
+        size = 0;
     }
 
+    public boolean add(E element) {
+        if(contains(element)) {
+            return false;
+        }
+        ensureCapacity(size + 1);
+        fillNullIndex(element);
+        return true;
+    }
+
+    //TODO - the toArray called in all these methods could be the toArray in this class. Ohhhhhh.
     public boolean addAll(Collection<? extends E> c) {
-        return false;
+        Object[] objects = c.toArray();
+        int oldSize = size;
+        ensureCapacity(size + objects.length);
+        for(Object o : objects) {
+            add((E) o);
+        }
+        return size != oldSize;
     }
 
     public void clear() {
-
+        mySet = new Object[MINIMUM_SIZE];
     }
 
     public boolean contains(Object o) {
+        if(o == null) {
+            return false; //TODO - what here?
+        } else if(mySet.length == 0) {
+            return false;
+        }
+
+        for(int i = 0; i < mySet.length; i++) {
+            if(o.equals(mySet[i])) {
+                return true;
+            }
+        }
+
         return false;
     }
 
@@ -32,16 +65,42 @@ public class MySet<E> implements Set<E> {
         return true;
     }
 
+    private void ensureCapacity(int minCapacity) {
+        if(minCapacity >= mySet.length) {
+            Object[] mySetCopy = new Object[minCapacity * 2];
+            for(int i = 0; i < size; i++) {
+                mySetCopy[i] = mySet[i];
+            }
+            mySet = mySetCopy;
+        }
+    }
+
     public boolean equals(Object o) {
         return false;
     }
 
+    private void fillNullIndex(E element) {
+        for(int i = 0; i < mySet.length; i++) {
+            if(mySet[i] == null) {
+                mySet[i] = element;
+                size++;
+                return;
+            }
+        }
+    }
+
     public int hashCode() {
-        return 0;
+        int hashCode = 0;
+        for(int i = 0; i < mySet.length; i++) {
+            if(mySet[i] != null) {
+                hashCode += mySet[i].hashCode();
+            }
+        }
+        return hashCode;
     }
 
     public boolean isEmpty() {
-        return false;
+        return size == 0;
     }
 
     public Iterator<E> iterator() {
@@ -49,6 +108,12 @@ public class MySet<E> implements Set<E> {
     }
 
     public boolean remove(Object o) {
+        for(int i = 0; i < mySet.length; i++) {
+            if(o.equals(mySet[i])) {
+                mySet[i] = null;
+                return true;
+            }
+        }
         return false;
     }
 
@@ -61,11 +126,18 @@ public class MySet<E> implements Set<E> {
     }
 
     public int size() {
-        return 0;
+        return size;
     }
 
     public Object[] toArray() {
-        return null;
+        Object[] mySetCopy = new Object[size];
+        int i = 0;
+        for(int j = 0; j < mySet.length; j++) {
+            if(mySet[j] != null) {
+                mySetCopy[i] = mySet[j];
+            }
+        }
+        return mySetCopy;
     }
 
     public <T> T[] toArray(T[] a) {
